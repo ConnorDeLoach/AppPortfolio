47961b37d1cc32fd052675894d79a39214456877
Connor DeLoach <connordeloach@gmail.com>
2016-04-01 01:39:20 -0400

    Feat: Wired activity_main Buttons to MainActivity

--- app/src/main/java/comconnordeloachmy_app_portfolio/github/www/appportfolio/MainActivity.java
+++ app/src/main/java/comconnordeloachmy_app_portfolio/github/www/appportfolio/MainActivity.java
@@ -1,7 +1,8 @@
 package comconnordeloachmy_app_portfolio.github.www.appportfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mSpotify = (Button) findViewById(R.id.spotify_button);
+        Button mScores = (Button) findViewById(R.id.scores_button);
+        Button mLibrary = (Button) findViewById(R.id.library_button);
+        Button mBuildb = (Button) findViewById(R.id.buildb_button);
+        Button mReader = (Button) findViewById(R.id.reader_button);
+        Button mMyApp = (Button) findViewById(R.id.my_app_button);
+
+
     }
 }
