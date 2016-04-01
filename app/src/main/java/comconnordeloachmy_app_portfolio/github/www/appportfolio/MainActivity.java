fabae7ca9adf47a54503c4930f0f7cace2649cc0
Connor DeLoach <connordeloach@gmail.com>
2016-04-01 11:04:21 -0400

    Feat: adds Toast to each button

--- app/src/main/java/comconnordeloachmy_app_portfolio/github/www/appportfolio/MainActivity.java
+++ app/src/main/java/comconnordeloachmy_app_portfolio/github/www/appportfolio/MainActivity.java
@@ -2,7 +2,9 @@ package comconnordeloachmy_app_portfolio.github.www.appportfolio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +21,37 @@ public class MainActivity extends AppCompatActivity {
         Button mMyApp = (Button) findViewById(R.id.my_app_button);
 
 
+    }
+
+    /**
+     * Method to receive button clicks from activity_main.xml
+     *
+     * @param v is the Button view
+     */
+    public void lnchAppBtn(View v) {
+
+        if (v.getId() == R.id.spotify_button) {
+            Toast.makeText(getApplicationContext(), "This button will launch my Spotify app!", Toast.LENGTH_SHORT).show();
+        }
+
+        if (v.getId() == R.id.scores_button) {
+            Toast.makeText(getApplicationContext(), "This button will launch my scores app!", Toast.LENGTH_SHORT).show();
+        }
+
+        if (v.getId() == R.id.library_button) {
+            Toast.makeText(getApplicationContext(), "This button will launch my library app!", Toast.LENGTH_SHORT).show();
+        }
+
+        if (v.getId() == R.id.buildb_button) {
+            Toast.makeText(getApplicationContext(), "This button will launch my build it bigger app!", Toast.LENGTH_SHORT).show();
+        }
+
+        if (v.getId() == R.id.reader_button) {
+            Toast.makeText(getApplicationContext(), "This button will launch my XYZ Reader app!", Toast.LENGTH_SHORT).show();
+        }
+
+        if (v.getId() == R.id.my_app_button) {
+            Toast.makeText(getApplicationContext(), "This button will launch my own personal app!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
